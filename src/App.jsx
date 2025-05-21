1930d0e83947fd41b82fd2dc7da7e6757483cc05
JustCat141 <patreszkaa@gmail.com>
2025-05-22 00:01:49 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+    return (
+        <main className="h-screen">
+            <header className="text-center text-3xl pageHeader">This is a header</header>
+        </main>
+    );
+}
+
+export default App;
